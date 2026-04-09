4d52b3b6ec841a6e0af093a12acaf21b4e14921c
Nikhildev-17 <komminenivenkatanikhil44@gmail.com>
2026-04-09 11:34:03 +0530

    Add validation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const main = require("./config/dataBaseConfig.js");
 const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 main();
 
 app.set("view engine", "ejs");
@@ -20,47 +21,62 @@ app.get("/", (req, res) => {
     res.send("Working");
 })
 
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
 
     const allListings = await Listing.find({});
     res.render("listings/index", {allListings});
 
-});
+}));
 
 app.get("/listings/new", (req, res) => {
     res.render("listings/forms.ejs");
 });
 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     let listingById = await Listing.findById(id);
     res.render("listings/show", {listingById});
-});
+}));
 
 app.post("/listings",  wrapAsync(async (req, res) => {
+    if(!req.body.listing){
+        throw new ExpressError(400, "Bad Request!");
+    }
     let newListing = req.body;
     let addNewListing = new Listing(newListing);
     await addNewListing.save();
     res.redirect("/listings");
 }));
 
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let {id} = req.params;
     let oldListing = await Listing.findById(id);
     res.render("listings/edit.ejs", {oldListing});
-});
+}));
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, req.body);
     res.redirect(`/listings/${id}`);
-});
+}));
 
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-});
+}));
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+})
+
+app.use((err, req, res, next) => {
+    if(err.name === "CastError"){
+        err = new ExpressError(400, "Invalid Id");
+    }
+    let {status = 500, message = "Something went wrong!"} = err;
+    res.status(status).send(message);
+})
 
 app.listen(3000, () => {
     console.log("Server running on port : 3000");
