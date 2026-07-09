cc32eac1959731b85f668a8012d3736c14cb4640
Nikhildev-17 <komminenivenkatanikhil44@gmail.com>
2026-07-09 11:12:54 +0530

    Authentication add

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const userRouter = require("./routes/user");
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing.js");
@@ -12,7 +13,13 @@ const main = require("./config/dataBaseConfig.js");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingsSchema } = require("./schema.js");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 main();
+
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -20,6 +27,32 @@ app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const sessionOptions = {
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
+app.use(session(sessionOptions));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
+    res.locals.currUser = req.user;
+    next();
+});
+
+
 
 const validateListing = (req, res, next) => {
     let { error } = listingsSchema.validate(req.body);
@@ -31,8 +64,10 @@ const validateListing = (req, res, next) => {
     }
 }
 
+app.use("/", userRouter);
+
 app.get("/", (req, res) => {
-    res.send("Working");
+    res.redirect("/listings");
 })
 
 app.get("/listings", wrapAsync(async (req, res) => {
---
+++ routes/users.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+
+const passport = require("passport");
+const User = require("../models/user");
+
+router.get("/signup", (req, res) => {
+    res.render("users/signup");
+});
+
+router.post("/signup", async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const newUser = new User({
+            email,
+            username,
+        });
+
+        const registeredUser = await User.register(newUser, password);
+
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+
+            res.redirect("/listings");
+        });
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+router.get("/login", (req, res) => {
+    res.render("users/login");
+});
+
+router.post(
+    "/login",
+    passport.authenticate("local", {
+        failureRedirect: "/login",
+    }),
+    (req, res) => {
+        res.redirect("/listings");
+    }
+);
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+
+        res.redirect("/login");
+    });
+});
+
+module.exports = router;
